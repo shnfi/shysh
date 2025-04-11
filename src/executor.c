6b3814580daf126328216b5a4e409732cb3835d7
shnfi <theshnfi@gmail.com>
2025-04-11 15:38:23 +0330

    changed the return type of the executor from void to int

--- src/executor.c
+++ src/executor.c
@@ -6,18 +6,29 @@
 #include "../utils/strcomp.h"
 #include "../utils/join.h"
 
-#include "../builtins/exit.h"
 #include "../builtins/echo.h"
 
-void exec(const char *prog, const char **switches, const unsigned int s)
+int exec(const char *prog, const char **switches, const unsigned int s)
 {
 	const char *ABSS = proc_switches(switches, s);
 
 	char *opt_cmd = lower(prog);
+	
+	/*
+	 * exit command
+	 */
+
 	if (str_comp(opt_cmd, "exit"))
-		quit();
+		return 0;
+
+	/*
+	 * other commands
+	 */
+
 	if (str_comp(opt_cmd, "echo"))
 		echo(join(switches, s));
 	else  
 		printf("INVALID COMMAND!\n");
+	
+	return 1;
 }
