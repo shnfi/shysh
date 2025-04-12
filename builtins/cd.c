939fd8fdfede97021d94f3735f0043705f3cdd02
shnfi <theshnfi@gmail.com>
2025-04-12 19:26:18 +0330

    cd builtin command init

---
+++ builtins/cd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "cd.h"
+
+#include "../utils/len.h"
+#include "../utils/strcpy.h"
+
+void cd(unsigned char *dir, const unsigned char *p)
+{
+	unsigned char *np = malloc(len(dir) + len(p));
+	str_cpy(np, dir);
+	unsigned int j = 0;
+
+	np[len(np)] = '/';
+
+	for (int i = len(dir); i < len(dir) + len(p); i++) {
+		np[i + 1] = p[j];
+		j++;
+	}
+
+	chdir(np);
+	getcwd(dir, 1024);
+}
---
+++ builtins/cd.h
@@ -0,0 +1,6 @@
+#ifndef CD_H
+#define CD_H
+
+void cd(unsigned char *dir, const unsigned char *p);
+
+#endif
