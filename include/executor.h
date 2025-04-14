6bb4aec32cded8d55532126067fd59c4b3b282a2
shnfi <theshnfi@gmail.com>
2025-04-14 17:29:14 +0330

    added 'raw_cmd' parameter and 'BUFFER' macro

--- include/executor.h
+++ include/executor.h
@@ -1,6 +1,8 @@
 #ifndef EXECUTOR_H
 #define EXECUTOR_H
 
-int exec(const char *prog, const char **switches, const unsigned int s, unsigned char *dir);
+#define BUFFER 2048
+
+int exec(const char *raw_cmd, const char *prog, const char **switches, const unsigned int s, unsigned char *dir);
 
 #endif
