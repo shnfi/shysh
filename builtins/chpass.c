0a4721108b16b8efe1bd272891fb166004c31cbb
shnfi <theshnfi@gmail.com>
2025-04-22 16:46:09 +0330

    removed the password validation process

--- builtins/chpass.c
+++ builtins/chpass.c
@@ -7,18 +7,11 @@
 
 void chpass(unsigned char *old_p)
 {
-	printf("Enter the old password: ");
-	
-	char tmp[MAX_PASS_BUFFER];
+	char tmp[MAX_PASS_BUFF];
+
+	printf("Enter the new password: ");
 	fgets(tmp, sizeof(tmp), stdin);
+	str_cpy(old_p, tmp);
 
-	if (str_comp(tmp, old_p)) {
-		printf("Enter a new password: ");
-		fgets(tmp, sizeof(tmp), stdin);
-		str_cpy(old_p, tmp);
-
-		printf("\nPassword has been changed!\n");
-	}
-	else
-		printf("Oh, don't you know the current password?!\n");
+	printf("\nPassword has been changed!\n");
 }
