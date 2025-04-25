3939dbce60bc58b208eed1547baeb31f3141bb0b
shnfi <theshnfi@gmail.com>
2025-04-25 15:48:32 +0330

    some modification on messages and errors

--- include/executor.h
+++ include/executor.h
@@ -2,7 +2,7 @@
 #define EXECUTOR_H
 
 #define BUFFER 2048
-#define WRONG_PASS "Wrong root password!"
+#define WRONG_PASS "[ERR] wrong root password!"
 
 int exec(const char *raw_cmd, const char *prog, const char **switches, const unsigned int s, unsigned char *dir, unsigned char *un, unsigned char *pass);
 
