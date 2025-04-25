1bb692370f7bd4ed13579b28ffb000ba37f6001b
shnfi <theshnfi@gmail.com>
2025-04-25 15:48:06 +0330

    added switch processing system

--- builtins/ls.c
+++ builtins/ls.c
@@ -3,12 +3,22 @@
 
 #include "ls.h"
 
+#include "../include/check_switch.h"
+
 void ls(const char *ABSS, unsigned char *dir)
 {
+	const unsigned char *valid_switches = "ltrha";
 	DIR *path = opendir(dir);
 	struct dirent *d = readdir(path);
 
-	while ((d = readdir(path)) != NULL) {
-		printf("- %s\n", d->d_name);
+	char *tmp = (char *) ABSS, c;
+	while (c = *tmp++) {
+		if (!check_switch(valid_switches, c)) {
+			printf("[ERROR] '%c' is not a valid switch for 'ls' command!\n", c);
+			return;
+		}
 	}
+
+	while ((d = readdir(path)) != NULL)
+		printf("- %s\n", d->d_name);
 }
